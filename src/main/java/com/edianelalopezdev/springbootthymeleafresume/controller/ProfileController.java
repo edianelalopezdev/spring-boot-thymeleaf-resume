3d6d5a35af7a403614bc15f5e6ed85337515438d
edianelalopezdev <edianelalopezdev@gmail.com>
2020-03-10 14:14:07 +0800

    An Update

--- src/main/java/com/edianelalopezdev/springbootthymeleafresume/controller/ProfileController.java
+++ src/main/java/com/edianelalopezdev/springbootthymeleafresume/controller/ProfileController.java
@@ -1,6 +1,5 @@
 package com.edianelalopezdev.springbootthymeleafresume.controller;
 
-import com.edianelalopezdev.springbootthymeleafresume.response.ProfileResource;
 import com.edianelalopezdev.springbootthymeleafresume.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@RequestMapping(value = "/profile")
 public class ProfileController {
 
     private final ProfileService profileService;
 
     @GetMapping
-    public ProfileResource getProfile() {
-        return profileService.getProfile();
-    }
-
-    @RequestMapping(value = "/")
-    public String createOrUpdateProfile() {
-
-        return "add-profile";
+    public String getProfile() {
+        return "view-profile";
     }
 }
